a1f9058cb52b1bdd19ff3e3dbc39b4ef9deb431a
Bagdat Yakushev <bagdat.yakushev@gmail.com>
2022-10-06 12:36:15 +0600

    changing status bar & system icons colors
    + from Dark to Light Theme and back

--- app/src/main/java/kz/bagiyak/statusbarsystemiconscolors/MainActivity.kt
+++ app/src/main/java/kz/bagiyak/statusbarsystemiconscolors/MainActivity.kt
@@ -1,11 +1,32 @@
 package kz.bagiyak.statusbarsystemiconscolors
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+
+            this.window.apply {
+                val isAppearanceLightStatusBar = WindowCompat.getInsetsController(this, this.decorView).isAppearanceLightStatusBars
+                if (isAppearanceLightStatusBar) {
+                    statusBarColor = android.graphics.Color.BLACK	// dark theme background
+                    // set system icons to White color
+                    WindowCompat.getInsetsController(this, this.decorView).isAppearanceLightStatusBars = false
+                    //old variant: ViewCompat.getWindowInsetsController(this.decorView)?.isAppearanceLightStatusBars = false
+                } else {
+                    statusBarColor = android.graphics.Color.WHITE	// light theme background
+                    // set system icons to Gray color
+                    WindowCompat.getInsetsController(this, this.decorView).isAppearanceLightStatusBars = true
+                    //old variant: ViewCompat.getWindowInsetsController(this.decorView)?.isAppearanceLightStatusBars = true
+                }
+            }
+        }
+
     }
 }
